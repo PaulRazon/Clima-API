d49941fb1d7b5a85f3fce072003f3463f08e5919
Bryan Paul Razón Machain <73920749+PaulRazon@users.noreply.github.com>
2024-02-07 21:15:23 -0700

    Proyecto finalizado-..

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -27,11 +27,11 @@ const ClimaProvider = ({children}) =>{//provee los hooks o funciones
             const {ciudad,pais}=datos
             const appId= import.meta.env.VITE_API_KEY
             const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad}
-            ,${pais}&limit=1&appid=79fc2300732ba1673184190d8688a1c8`
+            ,${pais}&limit=1&appid=${appId}`
             const {data} = await axios(url)
             const {lat,lon} =data[0]
 
-            const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=79fc2300732ba1673184190d8688a1c8}`
+            const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
 
             const {data: clima}=await axios(urlClima)
 
